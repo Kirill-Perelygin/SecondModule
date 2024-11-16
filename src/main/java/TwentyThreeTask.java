0033e3d6eda922328d5c3c5a2d407a046453e7a1
Kirill Perelygin <perelygin.kirill@gmail.com>
2024-11-16 19:15:39 +0300

    Adding new tasks

--- src/main/java/TwentyThreeTask.java
+++ src/main/java/TwentyThreeTask.java
@@ -1,2 +1,14 @@
 public class TwentyThreeTask {
-}
+    public static void main(String[] args) {
+        int firstBucket = 30;
+        int secondBucket = 0;
+        for( ; firstBucket > 0; ){
+            firstBucket = firstBucket - 1;
+            if (firstBucket % 10 == 0) continue;
+            System.out.println(firstBucket);
+            secondBucket = secondBucket + 1;
+            if (secondBucket % 10 == 0) continue;
+            System.out.println(secondBucket);
+            }
+        }
+    }
