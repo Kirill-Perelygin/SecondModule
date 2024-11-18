f527d1eed0d90786e4365327c97263c0f695cf09
Kirill Perelygin <perelygin.kirill@gmail.com>
2024-11-18 22:36:27 +0300

    Adding new tasks

--- src/main/java/Tamagotchi.java
+++ src/main/java/Tamagotchi.java
@@ -3,12 +3,15 @@ import java.util.Scanner;
 public class Tamagotchi {
 
     static String catName;
-    static int catWeight = 3;
+    static int catWeight = 10;
     static int catAge = 0;
     static int catHunger = 1;
     static int menuCallCount;
     static int catChoice;
     static int catActivity;
+    static int catCanFood = 10;
+    static int catFreshFood = 15;
+    static int catFood;
 
     public static void main(String[] args) {
 
@@ -20,6 +23,10 @@ public class Tamagotchi {
             System.out.println("Меню кота");
             catWeight -= 1;
             catHunger -= 1;
+            if (catWeight < 0 || catActivity < 0 || catHunger < 0) {
+                System.out.println("Вы проиграли");
+                break;
+            }
             System.out.println("Вес вашего кота составляет: " + catWeight);
             System.out.println("Голод вашего кота составляет: " + catHunger);
             System.out.println("Выберите желаемое дейстивие: ");
@@ -36,10 +43,41 @@ public class Tamagotchi {
                     if (catHunger >= 0 && catHunger <= 50){
                         System.out.println("Значение голода вашего кота составляет " + catHunger + ". Срочно покормите кота");}
                         else if (catHunger >= 51 && catHunger <= 100){
-                            System.out.println("Значение голода вашего кота составляет " + catHunger + ". Срочно покормите кота");
+                            System.out.println("Значение голода вашего кота составляет " + catHunger + ". Ваш не нуждается в еде");
+                        };
+                        if (catActivity >= 0 && catActivity <= 50){
+                            System.out.println("Ваш кот активен на: " + catActivity + " процентов. Срочно поиграйте с котом");
                         }
-                    System.out.println("Голод кота равняется: ");
+                        else if (catActivity >= 51 && catActivity <= 100){
+                            System.out.println("Ваш кот активен на: " + catActivity + " процентов. Ваш кот не нуждается в играх");
+                        }
                     break;
+                case (2):
+                    if (catHunger < 50) {
+                        System.out.println("Вес вашего кота составляет " + catHunger + " очков. Срочно покормите кота");
+                        System.out.println("Выберите чем покормить кота: ");
+                        System.out.println("1. Еда из банки");
+                        System.out.println("2. Свежая еда");
+                        Scanner catFoodScanner = new Scanner(System.in);
+                        catFood = catFoodScanner.nextInt();
+
+                        switch (catFood){
+                            case (1):
+                                catHunger += catCanFood;
+                                System.out.println("новый вес вашего кота равняется " + catHunger);
+                                break;
+                            case(2):
+                                catHunger += catFreshFood;
+                                System.out.println("новый вес вашего кота равняется " + catHunger);
+                                break;
+                            default:
+                                ;
+                        }
+                    }
+                        else if (catHunger >= 50 && catHunger <= 100) {
+                        System.out.println("Ваш кот сейчас не голоден");
+                    }
+
                     }
 
             }
